1584a9fefe4d9076dc7acc7d89baef85262d8ad8
Jeroen Mies <jeroen.mies@gmail.com>
2017-10-18 19:23:07 +0200

    Update state with fetched data

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,12 +6,9 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-    console.log(city);
-
     const url = `${ROOT_URL}&q=${city},nl`;
 
     const request = axios.get(url);
-    console.log(request);
 
     return {
         type: FETCH_WEATHER,
